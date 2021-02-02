b23fa41a6bf215a6328591b3daf1bce709b0798e
Hamtastical <meiliham1@gmail.com>
2021-02-02 09:27:34 -0500

    commit tree 00a04561ebc402d9c6239af420f7276d4fa2e66d

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,24 @@
 #write your code here
 
-def countdown
+def countdown_with_sleep(rest)
+    rest = sleep(5)
+
 end
+
+
+def countdown(number)
+      number = 10
+     celebrate = "HAPPY NEW YEAR!"
+        while number > 0
+            
+        puts "#{number} SECOND(S)!"
+        
+        number -= 1
+        if number == 0
+             return celebrate
+        
+        
+    end
+end
+end
+
